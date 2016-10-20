a0e53bd33615a49d5cbd26a65a676efd8aec6874
nmandica <nico.mandica@orange.fr>
2016-10-20 23:11:03 +0200

    Intersection with rectangle added

--- RayTracer/MainForm.cs
+++ RayTracer/MainForm.cs
@@ -16,8 +16,12 @@ namespace RayTracer
         public MainForm()
         {
             InitializeComponent();
+            var rectangle = new Rectangle(new Point3D(-1, 1, 10), new Point3D(3, 4, 10), new Point3D(4.2048, 2.3936, 10), new Point3D(0.2048, -0.6064, 10));
+            var ray = new Ray(new Vector3D(0, 2, 0), new Vector3D(0, -0.1, 1));
+            var intersection = new Vector3D(0, 0, 0);
 
-            MessageBox.Show(Rectangle.Exists(new Point3D(0, 0, 0), new Point3D(10, 0, 0), new Point3D(10, 5, 0), new Point3D(0, 5, 0)).ToString());
+            MessageBox.Show(rectangle.Intersects(ray, ref intersection).ToString());
+            MessageBox.Show(intersection.ToString());
         }
     }
 }
